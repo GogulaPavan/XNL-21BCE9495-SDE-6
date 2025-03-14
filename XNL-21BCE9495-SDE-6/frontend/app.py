232429b4403982741fe0ff66c9028b6be7aa9ced
PAVAN KUMAR GOGULA <143589457+GogulaPavan@users.noreply.github.com>
2025-03-14 14:59:22 +0530

    Update app.py

--- XNL-21BCE9495-SDE-6/frontend/app.py
+++ XNL-21BCE9495-SDE-6/frontend/app.py
@@ -1,3 +1,5 @@
+
+pip install -r requirements.txt
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
